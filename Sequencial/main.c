7fb757a3d1aae19e9c8001631b14a4fd93fd63e3
Christian2019 <chrisschmidt2702@gmail.com>
2020-05-20 20:49:23 -0300

    sequencial final

--- Sequencial/main.c
+++ Sequencial/main.c
@@ -3,9 +3,9 @@
 #include <math.h>
 #include <omp.h>
 
-# define NPOINTS 2000
-# define MAXITER 2000
 
+# define MAXITER 2000
+//export OMP_NUM_THREADS=1
 
 struct complex{
   double real;
@@ -13,11 +13,22 @@ struct complex{
 };
 
 int main(){
-  
-  int numoutside = 0;
+
   double area, error, ztemp;
-  double start, finish;
+  double start, finish,start2,finish2;
   struct complex z, c;
+  int i;
+  int j;
+  int iter;
+  start2 = omp_get_wtime();
+
+for (int NPOINTS=500; NPOINTS<5000; NPOINTS+=500){
+printf("NPOINTS = %1.2d \n",NPOINTS);
+for (int k=0; k<4; k++){
+printf("k = %1.2d \n",k);
+int numoutside = 0;
+
+  
 
 /*
  *
@@ -29,16 +40,27 @@ int main(){
 
   start = omp_get_wtime();
 
-  for (int i=0; i<NPOINTS; i++) {
-    for (int j=0; j<NPOINTS; j++) {
+
+  int i;
+  int j;
+  int iter;
+
+
+
+  for ( i=0; i<NPOINTS; i++) {
+    for ( j=0; j<NPOINTS; j++) {
+     
       c.real = -2.0+2.5*(double)(i)/(double)(NPOINTS)+1.0e-7;
       c.imag = 1.125*(double)(j)/(double)(NPOINTS)+1.0e-7;
       z=c;
-      for (int iter=0; iter<MAXITER; iter++){
-	ztemp=(z.real*z.real)-(z.imag*z.imag)+c.real;
+     
+      for (iter=0; iter<MAXITER; iter++){
+	
+     	ztemp=(z.real*z.real)-(z.imag*z.imag)+c.real;
 	z.imag=z.real*z.imag*2+c.imag;
 	z.real=ztemp;
 	if ((z.real*z.real+z.imag*z.imag)>4.0e0) {
+	 
 	  numoutside++;
 	  break;
 	}
@@ -58,5 +80,8 @@ int main(){
 
       printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n",area,error);
       printf("Time = %12.8f seconds\n",finish-start);
-
+}
+}
+finish2 = omp_get_wtime();
+printf("Total Time = %12.8f seconds\n",finish2-start2);
   }
