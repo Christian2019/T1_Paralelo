ab38b87f38e1fa62e0be54a01ace436943e26648
Christian2019 <chrisschmidt2702@gmail.com>
2020-05-20 00:03:45 -0300

    NPOINTS 500 EM 500

--- Paralela/main.c
+++ Paralela/main.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <omp.h>
 
-# define NPOINTS 2000
+//# define NPOINTS 2000
 # define MAXITER 2000
 
 
@@ -13,8 +13,10 @@ struct complex{
 };
 
 int main(){
+for (int NPOINTS=500; NPOINTS<5000; NPOINTS+=500){
+for (int x=0; x<4; x++){
 printf("\nOla 1 - FORA DA REGIAO PARALELA ...\n\n");
-    printf("teste\n");
+
   int numoutside = 0;
   double area, error, ztemp;
   double start, finish;
@@ -74,5 +76,6 @@ printf("Sou a thread %d de um total %d\n",id,nt);
 
       printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n",area,error);
       printf("Time = %12.8f seconds\n",finish-start);
-
+}
+}
   }
